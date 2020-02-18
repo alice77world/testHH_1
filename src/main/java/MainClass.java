4b50103f3bbd3d3e3d0a6e48cda27d86c6f7e920
alice77world <arapova.aygul@yandex.ru>
2020-02-18 20:07:45 +0500

    unit tests

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,37 +1,3 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.util.concurrent.TimeUnit;
-
 public class MainClass {
-    public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Айгуль\\IdeaProjects\\testHH\\drivers\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        driver.get("https://ufa.hh.ru/");
 
-        GeneralPage generalpage = new GeneralPage(driver);
-        LoginPage loginPage = generalpage.clickEnterButton();
-
-        HomePage homepage = loginPage.loginAs("arapovaalica@yandex.ru","12341234");
-        MakeResumePage makeresumepage = homepage.clickButtonCreateResume();
-
-        makeresumepage.clearFieldName(); //очищаем поля
-        makeresumepage.clearFieldSurname();
-        makeresumepage.clearFieldCity();
-
-        makeresumepage.typeName("1Test"); //это должны быть тесты
-        makeresumepage.typeName("Test-");
-        makeresumepage.typeName("-Test");
-        makeresumepage.typeName("Test-Test");
-        makeresumepage.typeName(" ");
-        makeresumepage.typeName("\n");
-        makeresumepage.typeName("\t");
-        makeresumepage.typeName("Test Test");
-
-    }
 }
--- src/main/java/MakeResumePage.java
+++ src/main/java/MakeResumePage.java
@@ -1,10 +1,8 @@
+import io.qameta.allure.Step;
+import org.apache.bcel.generic.Select;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-
-import java.security.Key;
-
-import static org.openqa.selenium.Keys.*;
 
 public class MakeResumePage {
     private final WebDriver driver;
@@ -17,34 +15,52 @@ public class MakeResumePage {
     By surnameLocator = By.xpath("//input[@name='lastName[0].string']");
     By telephoneLocator = By.xpath("//input[@name='phone[0].formatted']");
     By cityLocator = By.xpath("//input[@name='area[0].string']");
-    By dayBirthLocator = By.xpath("//input[@placeholder='День']");
-    By monthBirthLocator = By.xpath("//input[@placeholder='Месяц']");
-    By yearBirthLocator = By.xpath("//input[@placeholder='Год']");
-    By maleLocator = By.xpath("//input[@value='male']");
-    By femaleLocator = By.xpath("//input[@value='female']");
+    By dayLocator = By.xpath("//input[@name='birthday[0].date']");
+    By monthLocator = By.xpath("//select[@name='birthday[0].date']");
+
+    By errorNameLocator = By.xpath("//div[.='Только буквы и дефис']");
+    By errorTelephoneLocator = By.xpath("//div[.='Номер указан некорректно']");
+    By errorEmptyFieldLocator = By.xpath("//div[.='Обязательное поле']");
+    By errorDayLocator = By.xpath("//div[.='Некорректная дата']");
 
 
-
+    @Step
     public MakeResumePage typeName(String name) {
         driver.findElement(nameLocator).sendKeys(name);
         return this;
     }
 
+    @Step
     public MakeResumePage typeSurname(String surname) {
         driver.findElement(surnameLocator).sendKeys(surname);
         return this;
     }
 
+    @Step
     public MakeResumePage typeTelephone(String telephone) {
         driver.findElement(telephoneLocator).sendKeys(telephone);
         return this;
     }
 
+    @Step
     public MakeResumePage typeCity(String city) {
         driver.findElement(cityLocator).sendKeys(city);
         return this;
     }
 
+    @Step
+    public MakeResumePage typeDay(String name) {
+        driver.findElement(dayLocator).sendKeys(name);
+        return this;
+    }
+
+    @Step
+    public MakeResumePage selectMonth(String month) {
+        driver.findElement(monthLocator).sendKeys(month);
+        return this;
+    }
+
+    @Step
     public MakeResumePage clearFieldName() {
         driver.findElement(nameLocator).sendKeys(Keys.chord(Keys.CONTROL, Keys.HOME));
         driver.findElement(nameLocator).sendKeys(Keys.chord(Keys.CONTROL, Keys.SHIFT, Keys.END));
@@ -52,6 +68,7 @@ public class MakeResumePage {
         return this;
     }
 
+    @Step
     public MakeResumePage clearFieldSurname() {
         driver.findElement(surnameLocator).sendKeys(Keys.chord(Keys.CONTROL, Keys.HOME));
         driver.findElement(surnameLocator).sendKeys(Keys.chord(Keys.CONTROL, Keys.SHIFT, Keys.END,Keys.BACK_SPACE));
@@ -59,6 +76,7 @@ public class MakeResumePage {
         return this;
     }
 
+    @Step
     public MakeResumePage clearFieldCity() {
         driver.findElement(cityLocator).sendKeys(Keys.chord(Keys.CONTROL, Keys.HOME));
         driver.findElement(cityLocator).sendKeys(Keys.chord(Keys.CONTROL, Keys.SHIFT, Keys.END,Keys.BACK_SPACE));
@@ -66,24 +84,42 @@ public class MakeResumePage {
         return this;
     }
 
-    public boolean searchErrorName() {
-        boolean result = driver.findElement(By.linkText("Только буквы и дефис")).isDisplayed();
-        return result;
+    @Step
+    public MakeResumePage clearFieldTelephone() {
+        driver.findElement(telephoneLocator).sendKeys(Keys.chord(Keys.CONTROL, Keys.HOME));
+        driver.findElement(telephoneLocator).sendKeys(Keys.chord(Keys.CONTROL, Keys.SHIFT, Keys.END,Keys.BACK_SPACE));
+        driver.findElement(telephoneLocator).sendKeys(Keys.BACK_SPACE);
+        return this;
     }
 
-    public boolean searchErrorSurname() {
-        boolean result = driver.findElement(By.linkText("Только буквы и дефис")).isDisplayed();
-        return result;
+    @Step
+    public boolean isSearchErrorName() {
+        Boolean isPresent = driver.findElements(errorNameLocator).size() > 0;
+        return isPresent;
     }
 
-    public boolean searchErrorTelephone() { //что здесь делать?
-        boolean result = driver.findElement(By.linkText("Номер указан некорректно")).isDisplayed();
-        return result;
+    @Step
+    public boolean isSearchErrorEmptyField() {
+        Boolean isPresent = driver.findElements(errorEmptyFieldLocator).size() > 0;
+        return isPresent;
     }
 
-    public boolean searchErrorCity() {
-        boolean result = driver.findElement(By.linkText("Только буквы и дефис")).isDisplayed();
-        return result;
+    @Step
+    public boolean isSearchErrorTelephone() {
+        Boolean isPresent = driver.findElements(errorTelephoneLocator).size() > 0;
+        return isPresent;
+    }
+
+    @Step
+    public boolean isSearchErrorDay() {
+        Boolean isPresent = driver.findElements(errorDayLocator).size() > 0;
+        return isPresent;
+    }
+
+    @Step
+    public boolean isSearchErrorMonth() {
+        Boolean isPresent = driver.findElements(errorDayLocator).size() > 0;
+        return isPresent;
     }
 
 }
