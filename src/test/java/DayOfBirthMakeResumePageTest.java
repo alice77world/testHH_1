fdf8157c1bdaa0be9d646f2b9bf1b5d780f01c36
alice77world <arapova.aygul@yandex.ru>
2020-02-18 20:22:39 +0500

    unit tests

---
+++ src/test/java/DayOfBirthMakeResumePageTest.java
@@ -0,0 +1,57 @@
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.*;
+
+public class DayOfBirthMakeResumePageTest {
+
+    @Test
+    public void TooBigDayError() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Айгуль\\IdeaProjects\\testHH\\drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://ufa.hh.ru/");
+        GeneralPage generalpage = new GeneralPage(driver);
+        LoginPage loginPage = generalpage.clickEnterButton();
+        loginPage.typeUsername("arapovaalica@yandex.ru");
+        loginPage.typePassword("2251764");
+        HomePage homepage = loginPage.submitLogin();
+        MakeResumePage makeresumepage = homepage.clickButtonCreateResume();
+        makeresumepage.clearFieldName(); //очищаем поля
+        makeresumepage.clearFieldSurname();
+        makeresumepage.clearFieldCity();
+        makeresumepage.clearFieldTelephone();
+        makeresumepage.typeDay("123");
+        boolean actual = makeresumepage.isSearchErrorDay();
+        boolean expected = true;
+        assertEquals(expected,actual);
+        driver.quit();
+    }
+
+    @Test
+    public void DayWithoutError() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Айгуль\\IdeaProjects\\testHH\\drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://ufa.hh.ru/");
+        GeneralPage generalpage = new GeneralPage(driver);
+        LoginPage loginPage = generalpage.clickEnterButton();
+        loginPage.typeUsername("arapovaalica@yandex.ru");
+        loginPage.typePassword("2251764");
+        HomePage homepage = loginPage.submitLogin();
+        MakeResumePage makeresumepage = homepage.clickButtonCreateResume();
+        makeresumepage.clearFieldName(); //очищаем поля
+        makeresumepage.clearFieldSurname();
+        makeresumepage.clearFieldCity();
+        makeresumepage.clearFieldTelephone();
+        makeresumepage.typeDay("12");
+        boolean actual = makeresumepage.isSearchErrorDay();
+        boolean expected = false;
+        assertEquals(expected,actual);
+        driver.quit();
+    }
+
+}
